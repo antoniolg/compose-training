40f89d1c88dc568cd9af2047b9b7ad1993f8ebba
Antonio Leiva <antonioleivag@gmail.com>
2021-11-10 07:11:17 +0100

    Exercise: create button that shows a Toast

--- app/src/main/java/com/antonioleiva/composetraining/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/composetraining/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.antonioleiva.composetraining
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.antonioleiva.composetraining.ui.theme.ComposeTrainingTheme
 
@@ -19,13 +22,21 @@ class MainActivity : ComponentActivity() {
             ComposeTrainingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyButton(text = "Hello World")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyButton(text: String) {
+    val context = LocalContext.current
+    Button(onClick = { Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show() }) {
+        Text(text = text)
     }
 }
 
