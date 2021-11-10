8ed9054ad2d97cb66a3bd7081cbf08ea054b9835
Antonio Leiva <antonioleivag@gmail.com>
2021-11-10 13:09:27 +0100

    Exercise: Create LoginForm composable with Modifier

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -1,8 +1,11 @@
 package com.antonioleiva.composetraining.ui.screens.login
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
@@ -11,6 +14,7 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antonioleiva.composetraining.ui.screens.Screen
@@ -18,21 +22,29 @@ import com.antonioleiva.composetraining.ui.screens.Screen
 @Composable
 fun Login() {
     Screen {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.verticalScroll(rememberScrollState())
-        ) {
-            TextField(value = "user", onValueChange = {})
-            TextField(value = "password", onValueChange = {})
-            Button(onClick = { }) {
-                Text("Login")
-            }
+        LoginForm(modifier = Modifier
+            .wrapContentSize()
+            .background(Color.Gray.copy(alpha = 0.2f))
+            .padding(16.dp))
+    }
+}
+
+@Composable
+fun LoginForm(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.verticalScroll(rememberScrollState())
+    ) {
+        TextField(value = "user", onValueChange = {})
+        TextField(value = "password", onValueChange = {})
+        Button(onClick = { }) {
+            Text("Login")
         }
     }
 }
 
-@Preview("Login Light", heightDp = 100)
+@Preview("Login Light")
 @Preview("Login Dark", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun LoginPreview() {
