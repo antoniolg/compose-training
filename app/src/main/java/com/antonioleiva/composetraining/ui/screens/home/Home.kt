c42162284ea13dc4b00583ddc0dbfdaf6d15da3c
Antonio Leiva <antonioleivag@gmail.com>
2021-11-16 12:39:43 +0100

    Exercise: Use ListItem

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
@@ -1,12 +1,14 @@
 package com.antonioleiva.composetraining.ui.screens.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,39 +17,35 @@ import coil.transform.CircleCropTransformation
 import com.antonioleiva.composetraining.model.itemList
 import com.antonioleiva.composetraining.ui.screens.Screen
 
+@ExperimentalMaterialApi
 @Composable
 fun Home() {
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
         items(itemList) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Image(
-                    painter = rememberImagePainter(
-                        data = it.thumb,
-                        builder = {
-                            transformations(CircleCropTransformation())
-                            crossfade(true)
-                        }
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.size(48.dp)
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    Text(text = it.title)
-                    Text(text = it.subtitle)
+            ListItem(
+                text = { Text(it.title) },
+                secondaryText = { Text(it.subtitle) },
+                icon = {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = it.thumb,
+                            builder = {
+                                transformations(CircleCropTransformation())
+                                crossfade(true)
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.size(40.dp)
+                    )
                 }
-            }
+            )
         }
     }
 }
 
+@ExperimentalMaterialApi
 @Preview
 @Composable
 fun HomePreview() {
