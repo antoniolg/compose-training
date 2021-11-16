33e6456506e81f3c3dd4113e90b6f572334b4fab
Antonio Leiva <antonioleivag@gmail.com>
2021-11-16 12:28:48 +0100

    Load images for each item with Coil

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
@@ -1,15 +1,17 @@
 package com.antonioleiva.composetraining.ui.screens.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.antonioleiva.composetraining.model.itemList
 import com.antonioleiva.composetraining.ui.screens.Screen
 
@@ -19,11 +21,28 @@ fun Home() {
         modifier = Modifier.fillMaxSize()
     ) {
         items(itemList) {
-            Column(
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(16.dp)
             ) {
-                Text(text = it.title)
-                Text(text = it.subtitle)
+                Image(
+                    painter = rememberImagePainter(
+                        data = it.thumb,
+                        builder = {
+                            transformations(CircleCropTransformation())
+                            crossfade(true)
+                        }
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.size(48.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(text = it.title)
+                    Text(text = it.subtitle)
+                }
             }
         }
     }
