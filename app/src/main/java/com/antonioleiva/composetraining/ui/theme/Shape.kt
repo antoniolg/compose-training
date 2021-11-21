b59ce8b8e7c27702e677bd2bf6e21548f490b582
Antonio Leiva <antonio@devexperto.com>
2021-11-21 23:11:40 +0100

    Change Theme shapes

--- app/src/main/java/com/antonioleiva/composetraining/ui/theme/Shape.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/theme/Shape.kt
@@ -1,11 +1,12 @@
 package com.antonioleiva.composetraining.ui.theme
 
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    small = CutCornerShape(8.dp, 0.dp, 8.dp, 0.dp),
+    medium = CutCornerShape(16.dp),
+    large = CutCornerShape(32.dp)
 )
