e515d3c6649310f3ccaa4a5909ebe591c3e58557
antoniolg <antonio@devexperto.com>
2023-04-06 17:09:50 +0200

    Exercise: Create login form

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/Screen.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/Screen.kt
@@ -1,8 +1,10 @@
 package com.antonioleiva.composetraining.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.antonioleiva.composetraining.ui.theme.ComposeTrainingTheme
 
 @Composable
@@ -10,7 +12,7 @@ fun Screen(content: @Composable () -> Unit) {
     ComposeTrainingTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            //modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
             content()
--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -1,26 +1,32 @@
 package com.antonioleiva.composetraining.ui.screens.login
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.antonioleiva.composetraining.ui.screens.Screen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Login() {
     Screen {
-        MyButton(text = "Hello World")
-    }
-}
-
-@Composable
-fun MyButton(text: String) {
-    val context = LocalContext.current
-    Button(onClick = { Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show() }) {
-        Text(text = text)
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextField(value = "user", onValueChange = {})
+            TextField(value = "password", onValueChange = {})
+            Button(onClick = { }) {
+                Text("Login")
+            }
+        }
     }
 }
 
