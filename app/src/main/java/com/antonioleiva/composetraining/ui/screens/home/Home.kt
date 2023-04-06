913c75200390fe0097d6ab3f05fda216c9d2e235
antoniolg <antonio@devexperto.com>
2023-04-06 17:09:57 +0200

    Exercise: Add ViewModel to Home

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
@@ -4,28 +4,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.antonioleiva.composetraining.model.itemList
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.antonioleiva.composetraining.ui.screens.Screen
 
 @Composable
-fun Home() {
+fun Home(viewModel: HomeViewModel = viewModel()) {
     Screen {
-        var itemList by remember { mutableStateOf(itemList) }
         LazyColumn(
             modifier = Modifier.fillMaxSize()
         ) {
-            itemsIndexed(itemList) { index, item ->
-                CustomListItem(item = item, onAction = {
-                    val list = itemList.toMutableList()
-                    itemList = when (it) {
-                        Action.CLONE -> list.apply { add(index, item) }
-                        Action.DELETE -> list.apply { removeAt(index) }
-                    }
-                })
-                if (index < itemList.size - 1) {
+            itemsIndexed(viewModel.state) { index, item ->
+                CustomListItem(
+                    item = item,
+                    onAction = { viewModel.onAction(it, index) }
+                )
+                if (index < viewModel.state.size - 1) {
                     Divider()
                 }
             }
