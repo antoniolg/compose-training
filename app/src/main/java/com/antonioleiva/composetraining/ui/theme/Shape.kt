9cf0cea8e8bc91bb509d4418ff74380398acdad6
antoniolg <antonio@devexperto.com>
2023-04-06 17:10:05 +0200

    Change Theme shapes

---
+++ app/src/main/java/com/antonioleiva/composetraining/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.antonioleiva.composetraining.ui.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = CutCornerShape(8.dp, 0.dp, 8.dp, 0.dp),
+    small = CutCornerShape(16.dp),
+    medium = CutCornerShape(32.dp)
+)
--- app/src/main/java/com/antonioleiva/composetraining/ui/theme/Theme.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/theme/Theme.kt
@@ -62,6 +62,7 @@ fun ComposeTrainingTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
