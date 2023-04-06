03334fd5eaf49a5949376b578bed737c68717026
antoniolg <antonio@devexperto.com>
2023-04-06 17:10:11 +0200

    Update status bar theme

--- app/src/main/java/com/antonioleiva/composetraining/ui/theme/Theme.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/theme/Theme.kt
@@ -54,8 +54,8 @@ fun ComposeTrainingTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
