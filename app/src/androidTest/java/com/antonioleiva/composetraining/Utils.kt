656d64edb30e38e4499857c4fbd611dc1f818980
antoniolg <antonio@devexperto.com>
2023-04-06 17:10:16 +0200

    Example: Test scrollable Composables

--- app/src/androidTest/java/com/antonioleiva/composetraining/Utils.kt
+++ app/src/androidTest/java/com/antonioleiva/composetraining/Utils.kt
@@ -2,9 +2,7 @@ package com.antonioleiva.composetraining
 
 import android.content.Context
 import androidx.annotation.StringRes
-import androidx.compose.ui.test.SemanticsNodeInteraction
-import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
-import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.*
 import androidx.test.platform.app.InstrumentationRegistry
 
 val ctx: Context get() = InstrumentationRegistry.getInstrumentation().targetContext
@@ -20,3 +18,9 @@ fun SemanticsNodeInteractionsProvider.onNodeWithRes(
     ignoreCase,
     useUnmergedTree
 )
+
+fun hasContentDescription(
+    @StringRes res: Int,
+    substring: Boolean = false,
+    ignoreCase: Boolean = false
+): SemanticsMatcher = hasContentDescription(ctx.getString(res), substring, ignoreCase)
