d4a668f6f971e7c8a5405437dfb359ad6a470a98
antoniolg <antonio@devexperto.com>
2023-04-06 17:10:04 +0200

    Change font family

---
+++ app/src/main/java/com/antonioleiva/composetraining/ui/theme/Font.kt
@@ -0,0 +1,28 @@
+package com.antonioleiva.composetraining.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.antonioleiva.composetraining.R
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.montserrat_blackitalic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.montserrat_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.montserrat_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.montserrat_extrabold, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.montserrat_extrabolditalic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.montserrat_extralight, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.montserrat_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.montserrat_lightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.montserrat_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.montserrat_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.montserrat_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.montserrat_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.montserrat_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.montserrat_thinitalic, FontWeight.Thin, FontStyle.Italic),
+)
