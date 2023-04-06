18a994a452b27f7621e23217dd0eb8efe1f51db6
antoniolg <antonio@devexperto.com>
2023-04-06 17:09:51 +0200

    Exercise: Add scroll to Login Column

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -3,12 +3,15 @@ package com.antonioleiva.composetraining.ui.screens.login
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antonioleiva.composetraining.ui.screens.Screen
@@ -20,6 +23,7 @@ fun Login() {
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.verticalScroll(rememberScrollState())
         ) {
             TextField(value = "user", onValueChange = {})
             TextField(value = "password", onValueChange = {})
@@ -30,7 +34,7 @@ fun Login() {
     }
 }
 
-@Preview("Login Light")
+@Preview("Login Light", heightDp = 100)
 @Preview("Login Dark", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun LoginPreview() {
