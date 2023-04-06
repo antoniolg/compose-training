d8358d1a9c11c34b7fe10fba5c599a86f8da2bf8
antoniolg <antonio@devexperto.com>
2023-04-06 17:10:11 +0200

    Exercise: Create styled Button

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -88,7 +87,7 @@ private fun LoginForm(
             isError = isError,
             onDone = { if (isLoginEnabled) onSubmit(user, pass) }
         )
-        Button(
+        StyledButton(
             onClick = { onSubmit(user, pass) },
             enabled = isLoginEnabled
         ) {
---
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/StyledButton.kt
@@ -0,0 +1,49 @@
+package com.antonioleiva.composetraining.ui.screens.login
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+
+@Composable
+fun StyledButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.shape,
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val backgroundColor =
+        if (isPressed) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.tertiary
+
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor
+        ),
+        enabled = enabled,
+        shape = shape,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+        content = content
+    )
+}
