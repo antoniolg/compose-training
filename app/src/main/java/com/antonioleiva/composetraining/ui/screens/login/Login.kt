2bf15aa487eee8f621c70db86e32718da624d0ca
antoniolg <antonio@devexperto.com>
2024-09-10 16:23:09 +0200

    ViewModel for LoginScreen

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -1,7 +1,6 @@
 package com.antonioleiva.composetraining.ui.screens.login
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,40 +9,56 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.antonioleiva.composetraining.ui.theme.ComposeTrainingTheme
 
 @Composable
-fun Login(modifier: Modifier = Modifier) {
+fun Login(modifier: Modifier = Modifier, viewModel: LoginViewModel = viewModel()) {
+
+    val state = viewModel.state
+
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        val message = when {
+            state.loggedIn -> "Success"
+            state.error != null -> stringResource(id = state.error)
+            else -> null
+        }
+
         LoginForm(
             modifier = Modifier
                 .wrapContentSize()
                 .background(Color.Gray.copy(alpha = 0.2f))
-                .padding(16.dp)
+                .padding(16.dp),
+            message = message,
+            onSubmit = viewModel::loginClicked
         )
     }
 }
 
 @Composable
-private fun LoginForm(modifier: Modifier = Modifier) {
+private fun LoginForm(
+    modifier: Modifier = Modifier,
+    message: String? = null,
+    onSubmit: (user: String, pass: String) -> Unit
+) {
     var user by rememberSaveable { mutableStateOf("") }
     var pass by rememberSaveable { mutableStateOf("") }
     val isLoginEnabled = user.isNotBlank() && pass.isNotBlank()
@@ -64,13 +79,18 @@ private fun LoginForm(modifier: Modifier = Modifier) {
             label = { Text("Contraseña") }
         )
         Button(
-            onClick = {
-                user = ""
-                pass = ""
-            },
+            onClick = { onSubmit(user, pass) },
             enabled = isLoginEnabled
         ) {
             Text("Login")
+        }
+
+        if (message != null) {
+            Text(
+                text = message,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -80,6 +100,6 @@ private fun LoginForm(modifier: Modifier = Modifier) {
 @Composable
 fun LoginPreview() {
     ComposeTrainingTheme {
-        Login()
+        LoginForm(message = "This is message", onSubmit = { _, _ -> })
     }
 }
