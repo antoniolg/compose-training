c18764030b2698a15f08030d5712c3564cb24b97
antoniolg <antonio@devexperto.com>
2024-09-10 15:43:21 +0200

    Exercise: Create previews for Login

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -1,5 +1,6 @@
 package com.antonioleiva.composetraining.ui.screens.login
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.widget.Toast
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -7,6 +8,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.antonioleiva.composetraining.ui.theme.ComposeTrainingTheme
 
 @Composable
 fun Login(modifier: Modifier = Modifier) {
@@ -24,5 +27,14 @@ fun MyButton(text: String, modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         Text(text = text)
+    }
+}
+
+@Preview("Login Light")
+@Preview("Login Dark", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun LoginPreview() {
+    ComposeTrainingTheme {
+        Login()
     }
 }
