79c002c1b22fac7d5987e1b5523ef0961899d735
antoniolg <antonio@devexperto.com>
2024-09-10 15:45:17 +0200

    Exercise: Create login form

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/login/Login.kt
@@ -2,31 +2,33 @@ package com.antonioleiva.composetraining.ui.screens.login
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.antonioleiva.composetraining.ui.theme.ComposeTrainingTheme
 
 @Composable
 fun Login(modifier: Modifier = Modifier) {
-    MyButton(
-        text = "Hello World",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun MyButton(text: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    Button(
-        onClick = { Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show() },
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-        Text(text = text)
+        OutlinedTextField(value = "user", onValueChange = {})
+        OutlinedTextField(value = "password", onValueChange = {})
+        Button(onClick = { }) {
+            Text("Login")
+        }
     }
 }
 
