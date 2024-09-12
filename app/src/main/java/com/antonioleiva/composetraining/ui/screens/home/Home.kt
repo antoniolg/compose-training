73ca6368e5cf3b9d2f83248f6659292dd8414806
antoniolg <antonio@devexperto.com>
2024-09-12 07:11:12 +0200

    Exercise: Change home layout with the new icon

--- app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
+++ app/src/main/java/com/antonioleiva/composetraining/ui/screens/home/Home.kt
@@ -1,11 +1,21 @@
 package com.antonioleiva.composetraining.ui.screens.home
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ViewList
+import androidx.compose.material.icons.filled.GridView
+import androidx.compose.material.icons.filled.ViewList
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,14 +32,35 @@ fun Home(viewModel: HomeViewModel = viewModel()) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(items: List<Item>, onAction: (Action, Int) -> Unit) {
+    var gridMode by remember { mutableStateOf(false) }
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = stringResource(R.string.app_name)) }) }
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.app_name)) },
+                actions = {
+                    IconButton(onClick = { gridMode = !gridMode }) {
+                        Icon(
+                            imageVector = if (gridMode) Icons.AutoMirrored.Default.ViewList else Icons.Default.GridView,
+                            contentDescription = stringResource(R.string.change_view)
+                        )
+                    }
+                }
+            )
+        }
     ) { innerPadding ->
-        HomeList(
-            items = items,
-            onAction = onAction,
-            modifier = Modifier.padding(innerPadding)
-        )
+        if (gridMode) {
+            HomeGrid(
+                items = items,
+                onAction = onAction,
+                modifier = Modifier.padding(innerPadding)
+            )
+        } else {
+            HomeList(
+                items = items,
+                onAction = onAction,
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
 
